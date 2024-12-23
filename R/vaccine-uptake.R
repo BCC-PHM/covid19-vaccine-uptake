a61ed646f1c4362416f8ab276651939b21ec1c2c
David.Ellis <david.ellis@birmingham.gov.uk>
2024-12-23 18:18:32 +0000

    Added ethnicity dist and uptake by eth, age, and sex

--- R/vaccine-uptake.R
+++ R/vaccine-uptake.R
@@ -1,15 +1,14 @@
 source("R/config.R")
-library(readxl)
 library(dplyr)
 library(ggplot2)
 library(see)
 
 # Only load vaccine data if it's not already loaded
 if(!exists("vaccine_data")) {
-  vaccine_data <- read_excel(
+  vaccine_data <- read.csv(
     paste0(
       vaccine_data_path, 
-      "/covid_vaccinations-oct23_to_sept24.xlsx"
+      "/covid_vaccinations-oct23_to_sept24.csv"
     )
   ) %>%
     mutate(
@@ -31,14 +30,19 @@ if(!exists("vaccine_data")) {
                          "Any other white background") ~ "White",
         Ethnicity %in% c("Chinese","Any other ethnic group") ~ "Other",
         Ethnicity %in% c("Not stated") ~ "Unknown"
-      )
-    ) %>%
+      ),
+      # Fix capitalisation
+      Ethnicity = gsub("asian", "Asian", Ethnicity),
+      Ethnicity = gsub("black", "Black", Ethnicity),
+      Ethnicity = gsub("african", "African", Ethnicity),
+      Ethnicity = gsub("caribbean", "Caribbean", Ethnicity),
+    )%>%
     arrange(BroadEthnicity, Ethnicity)
-  
+  eth_order <- rev(unique(vaccine_data$Ethnicity))
   # Order by Broad ethnicity and alphabetical 
   vaccine_data$Ethnicity <- factor(
     vaccine_data$Ethnicity,
-    labels = rev(unique(vaccine_data$Ethnicity))
+    levels = eth_order
     )
 }
 
@@ -49,12 +53,20 @@ if(!exists("GP_reg_data")) {
       GP_data_path, 
       "/GP_reg_data_full_Dec_24.csv"
     )
-  )
+  ) %>%
+    mutate(
+      Ethnicity = case_when(
+        Ethnic_Description_National == "British" ~ "White British",
+        TRUE ~ Ethnic_Description_National
+      )
+    )
 }
 
 #################################################################
-#                 Analyse age distribution                      # 
+#           Uptake distributions (Age, ethnicity, IMD)          # 
 #################################################################
+
+##### Age #####
 
 # Plot age distribution of those receiving the Covid-19 vaccine
 female_data <- vaccine_data %>% filter(gender == "Female")
@@ -110,17 +122,156 @@ p
 ggsave("output/age_dist.png", p,
        width = 7, height = 10, dpi = 300)
 
-# Plot uptake rates by age group (< 65, 65-80, 80+), ethnicity, and LA
+##### Ethnicity #####
+
+p2 <- vaccine_data %>%
+  filter(gender %in% c("Female", "Male")) %>%
+  count(Ethnicity, gender) %>%
+  mutate(
+     plot_num = prettyNum(n, big.mark = ",", scientific = F)
+  ) %>%
+  ggplot( 
+    aes(y = Ethnicity, x = n, fill = gender)
+    ) +
+  geom_col(
+    position = "dodge",
+  ) +
+  geom_text(
+    aes(
+      x = n,
+      label = plot_num
+      ),
+    position = position_dodge(width = 1),
+    hjust = -0.15,
+    size = 3.5) +
+  theme_bw() +
+  labs(
+    x = "Number of Covid-19 Vaccines",
+    y = "",
+    fill = "",
+    title = "Ethnicty Distribution of Covid-19 Vaccine Recipients\n(October 23 – September 24)"
+  ) +
+  scale_fill_manual(values = c("#18981a", "#880990")) +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    legend.position = "top",
+  ) +
+  scale_x_continuous(
+    breaks = seq(0, 1.5e5, 0.5e5),
+    labels = prettyNum(seq(0, 1.5e5, 0.5e5), big.mark = ",",
+                       scientific = F),
+    lim = c(0, 1.5e5)
+  )
+p2
+ggsave("output/eth_dist.png", p2,
+       width = 6, height = 7, dpi = 300)
+
+#################################################################
+#           Uptake rates (Age, ethnicity, sex)          # 
+#################################################################
+
+# Plot uptake rates by age group (< 65, 65-80, 80+), ethnicity, and sex
 uptake_rates <- vaccine_data %>%
+  filter(
+    gender %in% c("Male", "Female")
+  ) %>%
   mutate(
     AgeBand = paste0(
       floor(age / 5) * 5,
       "-",
       floor(age / 5) * 5 + 4
-    )
-  ) %>%
+    ),
+    Sex = gender
+  ) %>% 
+  count(Ethnicity, BroadEthnicity, Sex, AgeBand) %>%
   left_join(
     GP_reg_data %>%
-      group_by(AgeBand, Ethnic_Description_National)
+      rename(Sex = Gender) %>%
+      group_by(AgeBand, Ethnicity, Sex) %>%
+      summarise(
+        N = sum(Observations, na.rm = T)
+      ),
+    by = join_by("AgeBand", "Ethnicity", "Sex")
+  ) %>%
+  filter(AgeBand != "NA-NA",
+         Ethnicity != "Not stated") %>%
+  mutate(
+    # Impute NA in N with 0
+    N = case_when(
+      is.na(N) ~ 0,
+      TRUE ~ N
+    ),
+    Age_Group = case_when(
+      AgeBand %in% c("65-69","70-74","75-79") ~ "65-79",
+      AgeBand %in% c("80-84", "85-89","90-94","95-99","100-104") ~ "80+",
+      TRUE ~ "Less than 65"
+    )
+  ) %>%
+  group_by(
+    Ethnicity, BroadEthnicity, Sex, Age_Group
+  ) %>%
+  summarise(
+    n = sum(n),
+    N_GP = sum(N),
+    p_GP = n/N_GP,
+    perc_GP = 100 * p_GP,
+    Z = qnorm(0.975),
+    LowerCI95 = 100 * n * (1 - 1/(9*n) - Z/3 * sqrt(1/(n+1)))**3/N_GP,
+    UpperCI95 = 100 * (n + 1) * (1 - 1/(9*(n + 1)) + Z/3 * sqrt(1/(n + 1)))**3/N_GP
   )
-  
+
+uptake_rates$Ethnicity <- factor(
+  uptake_rates$Ethnicity,
+  levels = eth_order
+)
+
+uptake_rates_age <- uptake_rates %>%
+  group_by(Age_Group) %>%
+  summarise(
+    n = sum(n),
+    N_GP = sum(N_GP),
+    p_GP = n/N_GP,
+    perc_GP = 100 * p_GP,
+    Z = qnorm(0.975),
+    LowerCI95 = 100 * n * (1 - 1/(9*n) - Z/3 * sqrt(1/(n+1)))**3/N_GP,
+    UpperCI95 = 100 * (n + 1) * (1 - 1/(9*(n + 1)) + Z/3 * sqrt(1/(n + 1)))**3/N_GP
+  )
+
+
+p3 <- ggplot(uptake_rates, aes(y = Ethnicity, x = perc_GP, fill = Sex)) +
+  geom_col(position = "dodge") +
+  geom_errorbar(
+    aes(xmin = LowerCI95, 
+        xmax = UpperCI95),
+    position = position_dodge(width = 1),
+    linewidth = 0.5,
+    width = 0.3
+  ) +
+  theme_bw() +
+  facet_wrap(
+    ~~factor(Age_Group, levels=c("Less than 65", "65-79", "80+")), 
+    ncol = 3, 
+    scales = "free_x"
+    ) +
+  geom_vline(
+    data = uptake_rates_age,
+    aes(xintercept = perc_GP,
+        linetype = "BSol Average")
+  ) +
+  labs(
+    y = "",
+    x = "Vaccine uptake %",
+    linetype = "",
+    title = "Vaccine Uptake % by Ethnicity, Age, and Sex\n(October 23 – September 24)"
+  ) +
+  scale_fill_manual(values = c("#18981a", "#880990")) +
+  scale_linetype_manual(values = c("dashed")) +
+  theme(
+    legend.position = "top",
+    plot.title = element_text(hjust = 0.5)
+  ) 
+
+p3
+
+ggsave("output/vaccine-uptake.png", p3,
+       width = 9, height = 7)
